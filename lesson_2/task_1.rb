338f30122202306bc8fd96ef5739d19a05b3430b
Dmitrii <veter.666@list.ru>
2018-07-19 22:15:46 +1000

    lesson 2 (2/6)

---
+++ lesson_2/task_1.rb
@@ -0,0 +1,16 @@
+months = {
+  "junuary" => 31,
+  "february" => 28,
+  "march" => 31,
+  "april" => 30,
+  "may" => 31,
+  "june" => 30,
+  "july" => 31,
+  "august" => 31,
+  "september" => 30,
+  "otcober" => 31,
+  "november" => 30,
+  "december" => 31
+}
+
+months.each { |month, amount_days| puts month if amount_days == 30 }
---
+++ lesson_2/task_2.rb
@@ -0,0 +1,11 @@
+numbers = []
+
+i = 10
+
+loop do
+  break if i > 100 
+  numbers << i
+  i += 5
+end  
+
+puts numbers
